2c5fa871589aab83b9ffd8e68a2968f4072a3eca
Martin Andronikos <69045805+martandrMC@users.noreply.github.com>
2020-08-01 16:30:41 +0300

    Started work Sockets

---
+++ src/martandrMC/jfilexc/protocol/socket/SocketHandler.java
@@ -0,0 +1,14 @@
+package martandrMC.jfilexc.protocol.socket;
+
+public class SocketHandler implements Runnable {
+
+	public SocketHandler() {
+	
+	}
+	
+	@Override
+	public void run() {
+	
+	}
+	
+}
